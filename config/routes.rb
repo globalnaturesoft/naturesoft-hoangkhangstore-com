c82023644517a2c366f0f74104cda83e56c7c9fb
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-09-20 00:31:45 -0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Naturesoft::Products::Engine.routes.draw do
     get "chuyen-muc-san-pham/:id.html" => "categories#list", as: :list_categories
     post "san-pham/xem-nhanh.html" => "products#quick_view", as: :quick_view
     get "san-pham/chi-tiet/:product_id.html" => "products#detail", as: :detail_products
+    get "gio-hang.html" => "carts#index", as: :carts
+    get "gio-hang/dang-nhap.html" => "carts#login", as: :cart_login
+    get "gio-hang/thong-tin-khach-hang.html" => "carts#customer_info", as: :cart_customer_info
+    get "gio-hang/thong-tin-giao-hang.html" => "carts#delivery_info", as: :cart_delivery_info
 end
 Naturesoft::Contacts::Engine.routes.draw do
     get "lien-he.html" => "contacts#contact", as: :contacts
