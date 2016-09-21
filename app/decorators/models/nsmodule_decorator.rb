c8bebbd06c51fc331340bc85d4401f9e88bb896b
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-09-20 20:57:51 -0700

    update footer module: company/account/support

--- app/decorators/models/nsmodule_decorator.rb
+++ app/decorators/models/nsmodule_decorator.rb
@@ -1,9 +1,14 @@
 Naturesoft::Nsmodule.class_eval do
   @hoangkhangstore_com_positions = {
     "box-vertical-megamenus" => nil,
-    "address-box" => nil,
     "homeslider" => nil,
     "header-banner" => nil,
     "category-slider" => nil,
+    
+    # position footer
+    "address-box" => nil,
+    "footer-column-2" => nil,
+    "footer-column-3" => nil,
+    "footer-column-4" => nil,
   }
 end
