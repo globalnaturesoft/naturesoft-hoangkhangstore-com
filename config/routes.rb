b47f1b96b55ad22f97aecc053280c3293964254a
Luan Pham <luanpm88@gmail.com>
2016-10-17 00:16:14 -0700

    update url friendly + page title home / category / product detail

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ end
 Naturesoft::Products::Engine.routes.draw do
     get "chuyen-muc-san-pham/:id.html" => "categories#list", as: :list_categories
     post "san-pham/xem-nhanh.html" => "products#quick_view", as: :quick_view
-    get "san-pham/chi-tiet/:product_id.html" => "products#detail", as: :detail_products
+    get "san-pham/(:title-):product_id.html" => "products#detail", as: :detail_products
     get "gio-hang.html" => "carts#index", as: :carts
     get "gio-hang/dang-nhap.html" => "carts#login", as: :cart_login
     get "gio-hang/thong-tin-khach-hang.html" => "carts#customer_info", as: :cart_customer_info
